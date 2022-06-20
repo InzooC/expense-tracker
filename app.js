06dd0ee1734c99f5e04b32daa71a21eb775ea083
InzooC <bluefreaks@gmail.com>
2022-06-20 10:00:20 +0800

    feat: add body-parser

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 const app = express()
 
@@ -8,9 +9,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 
-
 const port = process.env.PORT
-
 
 
 //設定引擎模板
@@ -19,7 +18,16 @@ app.set('view engine', 'handlebars')
 
 
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+
 app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
   res.render('index')
 })
 
