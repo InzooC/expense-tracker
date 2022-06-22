b9222cdd26914cab9c57dad8e1d2b523cf73e7c6
InzooC <bluefreaks@gmail.com>
2022-06-22 19:56:00 +0800

    feat: add filter record form category function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const Record = require('../../models/record')
+const Category = require('../../models/category')
 
 router.get('/', (req, res) => {
   Record.find()//拿出Record model所有東西
@@ -10,12 +11,42 @@ router.get('/', (req, res) => {
     .then(records => {
       let totalAmount = 0
       records.map(record => totalAmount += record.amount)
-      res.render('index', { records, totalAmount })
+      const showRecords = records
+
+      showRecords.map(record => {
+        Category.findOne({ id: record.categoryNumber })
+          .then(categoryData => {
+            record.icon = categoryData.name
+          })
+      })
+      return res.render('index', { showRecords, totalAmount })
     })
-    .catch(err => {
+    .catch(error => {
       console.error(error)
       res.render('errorPage', { error: '無法瀏覽首頁' })
     })
 })
+
+//選取特定類別
+router.post('/filter', (req, res) => {
+  const selectedCategoryValue = Number(req.body.category)
+  Record.find({ categoryNumber: selectedCategoryValue })
+    .lean()
+    .then(records => {
+      let totalAmount = 0
+      records.map(record => totalAmount += record.amount)
+      const showRecords = records
+
+      return res.render('index', { showRecords, totalAmount, selectedCategoryValue })
+    })
+    .catch(error => {
+      console.error(error)
+      res.render('errorPage', { error: '無法瀏覽首頁' })
+    })
+})
+
+
+
+
 
 module.exports = router
