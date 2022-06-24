c06ed11fc974b1f8f60655f72ee0a46f049f66bf
InzooC <bluefreaks@gmail.com>
2022-06-24 17:16:13 +0800

    feat: add login page

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -2,15 +2,26 @@ const express = require('express')
 const router = express.Router()
 
 
-// 瀏覽新增頁面
-router.get('/user/login', (req, res) => {
+// 瀏覽登入頁面
+router.get('/login', (req, res) => {
   res.render('login')
 })
-//瀏覽編輯頁面
+// 進行登入
+router.post('/login', (req, res) => {
+  res.redirect('/')
+})
+
+
+//瀏覽註冊頁面
 router.get('/register', (req, res) => {
   res.render('register')
 })
+// 進行註冊
+router.post('/register', (req, res) => {
+  res.redirect('/')
+})
 
+//帳號登出
 router.get('/logout', (req, res) => {
   req.logout()
   // req.flash('success_msg', '你已經成功登出。')   //還沒加flash功能
