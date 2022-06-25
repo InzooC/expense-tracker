0ab3e66517db944b02caa936f4ac27efcd35537c
InzooC <bluefreaks@gmail.com>
2022-06-25 22:12:27 +0800

    feat: add bcrypt to login route

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 
 module.exports = app => {
   // 初始化 Passport 模組
@@ -14,14 +15,21 @@ module.exports = app => {
           req.flash('failureLogin_msg', 'Email 還未被註冊！')
           return done(null, false)
         }
-        if (user.password !== password) {
-          req.flash('failureLogin_msg', '帳號或密碼不正確')
-          return done(null, false)
-        }
-        return done(null, user)
+        return bcrypt.compare(password, user.password)
+          .then(isMatch => {
+            if (!isMatch) {
+              req.flash('failureLogin_msg', '帳號或密碼不正確')
+              return done(null, false)
+            }
+            return done(null, user)
+          })
       })
-      .catch(error => done(error, false))
+      .catch(err => done(err, false))
   }))
+
+
+
+
   // 設定序列化與反序列化
   passport.serializeUser((user, done) => {
     done(null, user.id)
