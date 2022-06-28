337e2c7ebf85b58ed99f34e85b5dc0a916984696
InzooC <bluefreaks@gmail.com>
2022-06-28 14:21:44 +0800

    refactor: recordSeeder code

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -41,23 +41,26 @@ db.once('open', () => {
                 if (recordSerial.includes(record.id)) {
                   eachRecord.userId = user._id
                   //還不熟悉如何用promise包裝，所以還無法把CategoryModel的資料與record做關聯，現階段只能用手動的switch判斷式
-                  switch (record.categoryNumber) {
-                    case 1:
-                      eachRecord.icon = categories[0].icon;
-                      break;
-                    case 2:
-                      eachRecord.icon = categories[1].icon;
-                      break;
-                    case 3:
-                      eachRecord.icon = categories[2].icon;
-                      break;
-                    case 4:
-                      eachRecord.icon = categories[3].icon;
-                      break;
-                    case 5:
-                      eachRecord.icon = categories[4].icon;;
-                      break;
-                  }
+
+                  eachRecord.icon = categories[record.categoryNumber - 1].icon
+
+                  // switch (record.categoryNumber) {
+                  //   case 1:
+                  //     eachRecord.icon = categories[0].icon;
+                  //     break;
+                  //   case 2:
+                  //     eachRecord.icon = categories[1].icon;
+                  //     break;
+                  //   case 3:
+                  //     eachRecord.icon = categories[2].icon;
+                  //     break;
+                  //   case 4:
+                  //     eachRecord.icon = categories[3].icon;
+                  //     break;
+                  //   case 5:
+                  //     eachRecord.icon = categories[4].icon;;
+                  //     break;
+                  // }
                   userRecord.push(eachRecord)
                 }
               })
